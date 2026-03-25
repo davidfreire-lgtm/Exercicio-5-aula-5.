9561361ccdb93510c5e907d4eeb254875af5f8ba
davidfreire-lgtm <david.freire@escola.pr.gov.br>
2026-03-25 18:42:26 -0300

    Add files via upload

---
+++ SQLite.10.sql
@@ -0,0 +1,8 @@
+CREATE TABLE locadora(
+  codigo integer PRIMARY KEY AUTOINCREMENT,
+  titulo text,
+  genro text,
+  ano integer
+  );
+  
+  SELECT * FROM locadora;
---
+++ SQLite.11.sql
@@ -0,0 +1,9 @@
+INSERT INTO locadora( titulo, genro, ano)
+VALUES
+('vingadores ultimato', 'açao', 2019),
+('As Branquelas', 'comedia', 2004),
+('Toy Story', 'animaçao', 1995),
+('JUrassic park', 'aventura', 1993),
+('Interestelar', 'ficçao cientifica', 2014);
+
+SELECT * FROM locadora;
